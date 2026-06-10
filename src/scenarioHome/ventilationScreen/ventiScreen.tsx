028db527030db681e29a5ca29c501cf0374760e4
AggelosLadas <aggelosladas1313@gmail.com>
2026-06-10 18:27:46 +0300

    Added personalised on-line help buttons for Ventilation and Vitals Screen

--- src/scenarioHome/ventilationScreen/ventiScreen.tsx
+++ src/scenarioHome/ventilationScreen/ventiScreen.tsx
@@ -1,17 +1,30 @@
-import { Stack, Group, Button, Text, Center, rgba, Box } from '@mantine/core';
+import { Stack, Group, Button, Text, Center, rgba, Box, Modal } from '@mantine/core';
 import eventBus from '../../common/eventBus';
 import { useNavigate} from 'react-router';
 import { useClickOutside } from '@mantine/hooks';
 import { useScenarioContext } from '../scenarioHome';
 import { VitalWindow } from '../../common/VitalWindow';
+import { OnLineHelp, type PageHelp } from '../../common/onlineHelp';
 
 export default function VentilationScreen() {
     const context = useScenarioContext();
     const nav = useNavigate();
-    const ref = useClickOutside(() => nav(-1));
-
+    const { helpNeeded } = useScenarioContext();
+    const ref = useClickOutside(
+    () => {
+        // Only navigate back if the help modal is NOT open
+        if (!helpNeeded.value) {
+            nav(-1);
+        }
+    }, 
+    ['click'], 
+    [document.getElementById("medicineHelpBtn")]
+);
     return (
         <Center w="100%"  h="100%" bg={rgba('dark',0.6)}>
+            <Modal opened={helpNeeded.value} onClose={helpNeeded.toggle} title="Ventilation Screen">
+                                <OnLineHelp pageHelp={onlineHelp}></OnLineHelp>
+            </Modal>
             <Stack ref={ref} bg={'dark'} p={24} gap="md">
                 <VitalWindow label='SpO₂' state={context.vitals.spo2.state} value={context.vitals.spo2.value}></VitalWindow>
                 <VitalWindow label='RESPIRATORY RATE' state={context.vitals.rr.state} value={context.vitals.rr.value}></VitalWindow>
@@ -45,3 +58,41 @@ export default function VentilationScreen() {
         </Center>
     );
 }
+
+const onlineHelp: PageHelp = {
+    pageTitle: "Ventilator",
+    activeSections: [
+        {
+            title: "Οθόνη SpO2",
+            steps: [
+                {
+                    stepContent: "Η οθόνη του SpO2 δείχνει τα επίπεδα κορεσμού οξυγόνου στο αίμα του ασθενούς. Όταν η τιμή πέφτει εκτός των φυσιολογικών ορίων, η ένδειξη γίνεται κόκκινη. Αν παραμένει στο κανονικό της χρώμα, σημαίνει ότι η οξυγόνωση είναι καλή."
+                }
+            ]
+        },
+        {
+            title: "Οθόνη Αναπνευστικού Ρυθμού",
+            steps: [
+                {
+                    stepContent: "Στην οθόνη απεικονίζεται ο αριθμός των αναπνοών ανά λεπτό (RR). Εάν ο ρυθμός πέσει ή ανεβεί επικίνδυνα, η ένδειξη γίνεται κόκκινη για να σας προειδοποιήσει. Διαφορετικά, όλα βαίνουν καλώς."
+                }
+            ]
+        },
+        {
+            title: "Oxygen Density Controls",
+            steps: [
+                {
+                    stepContent: "Ρυθμίζει το ποσοστό οξυγόνου στο παρεχόμενο μείγμα αέρα (FiO2). Χρησιμοποιήστε τα κουμπιά για να αυξήσετε ή να μειώσετε τη συγκέντρωση ανάλογα με την κατάσταση του ασθενούς."
+                }
+            ]
+        },
+        {
+            title: "Oxygen Pumps Controls",
+            steps: [
+                {
+                    stepContent: "Ελέγχει τον ρυθμό ροής, τον όγκο ή την πίεση του αέρα. Προσαρμόστε την ένταση της αντλίας για να υποστηρίξετε επαρκώς την αναπνοή του ασθενούς."
+                }
+            ]
+        }
+    ]
+};
--- src/scenarioHome/vitalScreen/vitalScreen.tsx
+++ src/scenarioHome/vitalScreen/vitalScreen.tsx
@@ -1,20 +1,33 @@
 import { useClickOutside } from '@mantine/hooks';
 import eventBus from '../../common/eventBus';
-import { Button, Center, rgba, Stack } from '@mantine/core';
+import { Button, Center, Modal, rgba, Stack } from '@mantine/core';
 import { useNavigate} from 'react-router';
 import { useScenarioContext } from '../scenarioHome';
 import { VitalWindow } from '../../common/VitalWindow';
-
+import { OnLineHelp, type PageHelp } from '../../common/onlineHelp';
 
 export default function VitalScreen() {
     const context = useScenarioContext();
     const nav = useNavigate();
 
-    const ref = useClickOutside(() => nav(-1));
+    const { helpNeeded } = useScenarioContext();
+    const ref = useClickOutside(
+    () => {
+        // Only navigate back if the help modal is NOT open
+        if (!helpNeeded.value) {
+            nav(-1);
+        }
+    }, 
+    ['click'], 
+    [document.getElementById("medicineHelpBtn")]
+);
     
 
     return (
         <Center w="100%"  h="100%" bg={rgba('dark',0.6)}>
+            <Modal opened={helpNeeded.value} onClose={helpNeeded.toggle} title="Vitals Screen">
+                                            <OnLineHelp pageHelp={onlineHelp}></OnLineHelp>
+                        </Modal>
             <Stack ref={ref} bg={'dark'} p={24} gap="md">
                 <VitalWindow label='heart rate' state={context.vitals.hr.state} value={context.vitals.hr.value}></VitalWindow>
                 <VitalWindow label='blood preasure' state={context.vitals.bp.state} value={context.vitals.bp.value}></VitalWindow>
@@ -31,4 +44,41 @@ export default function VitalScreen() {
     );
 }
 
+const onlineHelp: PageHelp = {
+    pageTitle: "Vitals Screen",
+    activeSections: [
+        {
+            title: "Heart Rate (Καρδιακός Ρυθμός)",
+            steps: [
+                {
+                    stepContent: "Η οθόνη δείχνει τους καρδιακούς παλμούς του ασθενούς. Αν ο ρυθμός πέσει ή ανεβεί εκτός των φυσιολογικών ορίων, η ένδειξη γίνεται κόκκινη. Διαφορετικά, όλα βαίνουν καλώς."
+                }
+            ]   
+        },
+        {
+            title: "Blood Pressure (Αρτηριακή Πίεση)",
+            steps: [
+                {
+                    stepContent: "Απεικονίζει την αρτηριακή πίεση. Όταν η πίεση πέσει ή ανεβεί επικίνδυνα, η ένδειξη γίνεται κόκκινη για να σας προειδοποιήσει. Αν παραμένει στο κανονικό της χρώμα, σημαίνει ότι η πίεση είναι σε καλά επίπεδα."
+                }
+            ]
+        },
+        {
+            title: "Temperature (Θερμοκρασία)",
+            steps: [
+                {
+                    stepContent: "Δείχνει τη θερμοκρασία του σώματος του ασθενούς. Εάν η θερμοκρασία αυξηθεί ή μειωθεί σημαντικά, η ένδειξη θα γίνει κόκκινη."
+                }
+            ]
+        },
+        {
+            title: "Shock & Shock Light",
+            steps: [
+                {
+                    stepContent: "Κουμπιά για τη χορήγηση απινίδωσης. Χρησιμοποιήστε το 'Shock' για κανονική ένταση και το 'Shock Light' για πιο ήπια παρέμβαση, ανάλογα με το επείγον της κατάστασης του ασθενούς."
+                }
+            ]
+        }
+    ]
+};
 
