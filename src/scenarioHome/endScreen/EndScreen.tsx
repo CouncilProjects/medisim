ddbcc7c37b80094247d38372e3a5a7d55854e87a
amorianos <amorianosdimitris@gmail.com>
2026-06-29 18:02:51 +0300

    gree end screen

--- src/scenarioHome/endScreen/EndScreen.tsx
+++ src/scenarioHome/endScreen/EndScreen.tsx
@@ -1,168 +1,209 @@
 import { useEffect } from "react"
-import { Accordion, Box, Button, Card,Center,Divider,Group,Image,List,Loader,Modal,NumberFormatter,Stack,Text, Timeline, TimelineItem } from "@mantine/core";
+import { Accordion, Box, Button, Card, Center, Divider, Group, Image, List, Loader, Modal, NumberFormatter, Stack, Text, Timeline, TimelineItem } from "@mantine/core";
 import type { Action } from "../../engine/schemas/actionEnum";
 import { useLocation, useNavigate } from "react-router";
 import engine from "../../engine/engine";
 import { OnLineHelp, type PageHelp } from "../../common/onlineHelp";
 import { useAppContext } from "../../App";
-import { downloadJSON,downloadPDF } from "./downloadFunctions";
+import { downloadJSON, downloadPDF } from "./downloadFunctions";
 
+export function EndScreen() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { helpNeeded } = useAppContext();
 
-export function EndScreen(){
-    const location = useLocation();
-    const navigate = useNavigate();
-    const { helpNeeded } = useAppContext();
-   
-    const scen = location.state?.scenario;
+  const scen = location.state?.scenario;
 
-    useEffect(()=>{
-        if(scen==undefined){
-            navigate("/home", { replace: true });
-        }
-    },[scen]);
-
-    const debrif = scen ? engine.getDebrief(scen) : null;
-
-    useEffect(() => {
-        window.history.pushState(null, '', window.location.href);
-
-        const handlePop = () => {
-            navigate('/home', { replace: true });
-        };
-
-        window.addEventListener('popstate', handlePop);
-        return () => window.removeEventListener('popstate', handlePop);
-    }, [navigate]);
-
-    useEffect(() => {
-        // @ts-ignore
-        const handleBeforeUnload = (event) => {
-            event.preventDefault();
-            // Some browsers require returnValue for the dialog to show
-            event.returnValue = "";
-        };
-
-        window.addEventListener("beforeunload", handleBeforeUnload);
-
-        return () => {
-            window.removeEventListener("beforeunload", handleBeforeUnload);
-        };
-    }, []);
-
-    if(debrif==null){
-        return <Loader></Loader>
+  useEffect(() => {
+    if (scen == undefined) {
+      navigate("/home", { replace: true });
     }
+  }, [scen]);
 
-    const now = new Date();
-    const dateTOprint =
-        now.getFullYear() + "-" +
-        String(now.getMonth() + 1).padStart(2, "0") + "-" +
-        String(now.getDate()).padStart(2, "0") + " " +
-        String(now.getHours()).padStart(2, "0") + "-" +
-        String(now.getMinutes()).padStart(2, "0");
+  const debrif = scen ? engine.getDebrief(scen) : null;
 
-    return <Center w={'100%'} h={'100%'}>
-        <Card p={12} m={4}>
-            <Group justify="space-between">
-                <Text fw={1000}>Your report</Text>
-                
-                <Group>
-                    <Button leftSection={<Image src={"/download.svg"} w={32} h={32}></Image>} onClick={() => { downloadJSON(debrif) }}>Download .json</Button>
-                    <Button leftSection={<Image src={"/download.svg"} w={32} h={32}></Image>} onClick={() => { downloadPDF(debrif,dateTOprint) }}>Download .pdf</Button>
-                </Group>
-            </Group>
-            <Stack>
-                <Text>Scenario : {debrif.scenarioName}</Text>
-                <Text>Test taker : {debrif.taker}</Text>
-                <Text>Final score : {debrif.score}</Text>
-                <Text>Accuracy : <NumberFormatter value={debrif.goodPercent} decimalScale={2} suffix="%" /></Text>
-            </Stack>
-            <Divider></Divider>
-            <Text>Timeline</Text>
-             
-            <Box mah={"60dvh"} style={{overflowY:'auto'}}>
-                <Timeline active={debrif.timeline.length}>
-                    {
-                        debrif.timeline.map(nodeLine => {
-                            return (
-                                <TimelineItem title={nodeLine.duringNode}>
-                                    <Text>{nodeLine.nodeText}</Text>
-                                    <Accordion>
-                                        <Accordion.Item key={nodeLine.duringNode} value={"Detailes"}>
-                                            <Accordion.Control>Detailes</Accordion.Control>
-                                            <Accordion.Panel>
-                                                <List>
-                                                    {
-                                                        nodeLine.nodeTimeline.map(act => {
-                                                            return <List.Item><Text>{act.valid ? '✅' : '❌'} [{act.action}] caused score to change by {act.scoreDelta}</Text></List.Item>
-                                                        })
-                                                    }
-                                                </List>
-                                            </Accordion.Panel>
-                                           
-                                        </Accordion.Item>
-                                        
-                                    </Accordion>
-                                </TimelineItem>
-                            )
-                        })
-                    }
-                </Timeline>
-            </Box>
-        </Card>
-         <Modal opened={helpNeeded.value} onClose={helpNeeded.toggle} title="On-line help">
-            <OnLineHelp pageHelp={onlineHelp}></OnLineHelp>
-        </Modal>
+  useEffect(() => {
+    window.history.pushState(null, '', window.location.href);
+
+    const handlePop = () => {
+      navigate('/home', { replace: true });
+    };
+
+    window.addEventListener('popstate', handlePop);
+    return () => window.removeEventListener('popstate', handlePop);
+  }, [navigate]);
+
+  useEffect(() => {
+    // @ts-ignore
+    const handleBeforeUnload = (event) => {
+      event.preventDefault();
+      event.returnValue = "";
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
+
+  if (debrif == null) {
+    return <Loader></Loader>
+  }
+
+  const now = new Date();
+  const dateTOprint =
+    now.getFullYear() + "-" +
+    String(now.getMonth() + 1).padStart(2, "0") + "-" +
+    String(now.getDate()).padStart(2, "0") + " " +
+    String(now.getHours()).padStart(2, "0") + "-" +
+    String(now.getMinutes()).padStart(2, "0");
+
+  return (
+    <Center w={'100%'} h={'100%'}>
+      <Card p={12} m={4}>
+        <Group justify="space-between">
+          <Text fw={1000}>Αναφορά Αποτελεσμάτων</Text>
+
+          <Group>
+            <Button
+              leftSection={<Image src={"/download.svg"} w={32} h={32} />}
+              onClick={() => { downloadJSON(debrif) }}
+            >
+              Λήψη .json
+            </Button>
+
+            <Button
+              leftSection={<Image src={"/download.svg"} w={32} h={32} />}
+              onClick={() => { downloadPDF(debrif, dateTOprint) }}
+            >
+              Λήψη .pdf
+            </Button>
+          </Group>
+        </Group>
+
+        <Stack>
+          <Text>Σενάριο: {debrif.scenarioName}</Text>
+          <Text>Εκπαιδευόμενος: {debrif.taker}</Text>
+          <Text>Τελική Βαθμολογία: {debrif.score}</Text>
+          <Text>
+            Ποσοστό Επιτυχίας:{" "}
+            <NumberFormatter
+              value={debrif.goodPercent}
+              decimalScale={2}
+              suffix="%"
+            />
+          </Text>
+        </Stack>
+
+        <Divider />
+
+        <Text>Χρονογραμμή</Text>
+
+        <Box mah={"60dvh"} style={{ overflowY: 'auto' }}>
+          <Timeline active={debrif.timeline.length}>
+            {
+              debrif.timeline.map(nodeLine => {
+                return (
+                  <TimelineItem title={nodeLine.duringNode}>
+                    <Text>{nodeLine.nodeText}</Text>
+
+                    <Accordion>
+                      <Accordion.Item
+                        key={nodeLine.duringNode}
+                        value={"Λεπτομέρειες"}
+                      >
+                        <Accordion.Control>
+                          Λεπτομέρειες
+                        </Accordion.Control>
+
+                        <Accordion.Panel>
+                          <List>
+                            {
+                              nodeLine.nodeTimeline.map(act => {
+                                return (
+                                  <List.Item>
+                                    <Text>
+                                      {act.valid ? '✅' : '❌'} [{act.action}] προκάλεσε μεταβολή της βαθμολογίας κατά {act.scoreDelta}
+                                    </Text>
+                                  </List.Item>
+                                )
+                              })
+                            }
+                          </List>
+                        </Accordion.Panel>
+
+                      </Accordion.Item>
+
+                    </Accordion>
+                  </TimelineItem>
+                )
+              })
+            }
+          </Timeline>
+        </Box>
+      </Card>
+
+      <Modal
+        opened={helpNeeded.value}
+        onClose={helpNeeded.toggle}
+        title="Ηλεκτρονική Βοήθεια"
+      >
+        <OnLineHelp pageHelp={onlineHelp} />
+      </Modal>
     </Center>
-
-
+  );
 }
 
-export type Debrief ={
-    taker:string,
-    scenarioName:string,
-    score:number,
-    goodPercent:number,
-    timeline:NodeTimelineSnapshot[]
+export type Debrief = {
+  taker: string,
+  scenarioName: string,
+  score: number,
+  goodPercent: number,
+  timeline: NodeTimelineSnapshot[]
 }
-
-
 
 export type NodeTimelineSnapshot = {
-    duringNode:string,
-    nodeText:string,
-    nodeTimeline:ActionSnapshot[]
+  duringNode: string,
+  nodeText: string,
+  nodeTimeline: ActionSnapshot[]
 }
 
 export type ActionSnapshot = {
-    action:Action,
-    valid:boolean,
-    scoreDelta:number
+  action: Action,
+  valid: boolean,
+  scoreDelta: number
 }
 
-const onlineHelp:PageHelp = {
-    pageTitle:"Report page",
-    activeSections:[
+const onlineHelp: PageHelp = {
+  pageTitle: "Σελίδα Αναφοράς",
+  activeSections: [
+    {
+      title: "Χρονογραμμή",
+      steps: [
         {
-            title:"Timeline",
-            steps:[
-                {stepContent:"The timeline shows what path was taken"},
-                {stepContent:"Pressing on 'details' brings up the actions taken and how they affected the score"}
-            ]
+          stepContent: "Η χρονογραμμή παρουσιάζει τη διαδρομή που ακολουθήθηκε κατά την εκτέλεση του σεναρίου."
         },
         {
-            title: ".json download",
-            steps: [
-                { stepContent: "Download the report in json format" },
-            ]
-        },
-        {
-            title: ".pdf download",
-            steps: [
-                { stepContent: "Download the report in pdf format" },
-            ]
+          stepContent: "Πατώντας «Λεπτομέρειες» εμφανίζονται οι ενέργειες που πραγματοποιήθηκαν και ο τρόπος με τον οποίο επηρέασαν τη βαθμολογία."
         }
-    ]
+      ]
+    },
+    {
+      title: "Λήψη .json",
+      steps: [
+        {
+          stepContent: "Κατεβάστε την αναφορά σε μορφή JSON."
+        }
+      ]
+    },
+    {
+      title: "Λήψη .pdf",
+      steps: [
+        {
+          stepContent: "Κατεβάστε την αναφορά σε μορφή PDF."
+        }
+      ]
+    }
+  ]
 }
-
-
